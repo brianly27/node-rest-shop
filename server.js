6899f3893bccea3cf5d2616e6b33098b301c3bca
brianly27 <brian.kyang.ly@gmail.com>
2020-07-03 09:36:16 -0700

    node can launch server.js

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const http = require('http')
 //after creating app.js, import app and pass into createServer()
 const app = require('./app')
 
-const port = process.env.PORT || 3000
+const port = 3000
 
-const server = http.createServer()
+const server = http.createServer(app)
 
 //server.listen starts the server and passes the port as an argument
 //afterwards, the server will execute the function that we passed into createSever()
